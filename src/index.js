7c8079abdb0c120e25d00ededc8d5f8d82d343a8
Goldobin Anton <722451@it-atlantic.com>
2019-11-04 03:06:57 +0300

    asos first page data

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ const cheerio = require('cheerio');
 
 
 let pagesData = []
-const generalUrl = 'https://youthstore.ru/'
-const startThemeUrl = 'c/men/clothes/sweatshirts'
+const generalUrl = 'https://www.asos.com/'
+const startThemeUrl = 'ru/men/tufli-botinki-i-kedy/krossovki/cat/?cid=5775'
 
-
-
-const nightmare = Nightmare({ show: true });
-nightmare
-  .goto(generalUrl + startThemeUrl)
-  .wait(500)
+const nightmare = Nightmare({ show: true});
+nightmare.goto(generalUrl + startThemeUrl)
+  .wait(3000)
+  .evaluate(function() {
+    window.document.body.scrollTop = document.body.scrollHeight;
+  })
+  .scrollTo(5000, 0)
+  .wait('#product-12489036 > a > div > img[data-auto-id="productTileImage"]')
   .evaluate(function () {
     return document.documentElement.outerHTML;
   })
@@ -20,14 +22,13 @@ nightmare
     let getData = html => {
       const $ = cheerio.load(html, { decodeEntities: false })
 
-
       //find all items and get title, info and price
-      $("#items-holder li").each((i, elem) => {
+      $("._3-pEc3l article").each((i, elem) => {
         pagesData.push({
-          title: $(elem).find('.title').text(),
-          img: generalUrl + $(elem).find('.image img').attr('src'),
-          price: $(elem).find('.price').text(),
-          url: generalUrl + $(elem).find('a').attr('href')
+          title: $(elem).find('._1Ai1lKq p').text(),
+          img: 'https:' + $(elem).find('._3x-5VWa img').attr('src'),
+          price: $(elem).find('._3b3kqA8').text(),
+          url: generalUrl + $(elem).find('._3x-5VWa').attr('href')
         })
 
       });
